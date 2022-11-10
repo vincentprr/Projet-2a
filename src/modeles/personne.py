04d37879da5847627a3c03c20efdddb267c253ec
vincentprr <vincentprr@users.noreply.github.com>
2022-11-10 10:01:59 +0100

    fix: Deletion of unuse character in `footer.html`

--- src/modeles/personne.py
+++ src/modeles/personne.py
@@ -2,7 +2,7 @@ from core.database import db
 from sqlalchemy.dialects.mysql import TINYINT
 
 class Personne(db.Model):
-    idP = db.Column(db.Integer, primary_key=True)
+    id = db.Column("idP", db.Integer, primary_key=True)
     nomP = db.Column(db.String(42))
     prenomP = db.Column(db.String(42))
     typeId = db.Column(TINYINT)
