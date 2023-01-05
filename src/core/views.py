ae4f62b7dc273ecb075f189704c61106b0812ed3
Bastian <rapine.bastian@gmail.com>
2023-01-05 10:32:43 +0100

    feat: creation de la rout pour acceder à la feuille de route

--- src/core/views.py
+++ src/core/views.py
@@ -34,3 +34,7 @@ def inscription():
 @app.route('/gestion')
 def gestion():
     return render_template("gestion.html")
+
+@app.route('/feuille_de_route')
+def feuille_de_route():
+    return render_template("feuilleRoute.html")
