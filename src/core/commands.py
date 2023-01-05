8b346e5b0fc989bd9fbb816682f4c5739ef3e89b
vincentprr <vincentprr@users.noreply.github.com>
2023-01-05 09:48:31 +0100

    feat: adding a register function

--- src/core/commands.py
+++ src/core/commands.py
@@ -1,5 +1,6 @@
 from .app import app
 from .database import db, create_tables
+import click
 
 @app.cli.command()
 def syncdb():
@@ -7,3 +8,13 @@ def syncdb():
     Create all table based on the models
     """
     create_tables(db)
+
+@app.cli.command()
+@click.argument("username")
+@click.argument("password")
+@click.argument("user_type_id")
+def createUser(username, password, user_type_id):
+    """
+    Create a new user
+    """
+    pass
