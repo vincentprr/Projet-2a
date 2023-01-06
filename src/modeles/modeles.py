7755351dc752c11a83515abde69765d731635b2d
vincentprr <vincentprr@users.noreply.github.com>
2023-01-06 10:54:50 +0100

    fix: Adding simple modeles

--- src/modeles/modeles.py
+++ src/modeles/modeles.py
@@ -1,3 +1,3 @@
 # File to register all modeles for the creation of the tables #
 
-from . import personne, appartient, auteur, regime, avoir, cle, creneau, dedicacer, exposant, hotel, intervenant, loger, maison_edition, mangeur, navette, personne, repas, restaurant, staff, transporter, travailler, voyage, manger
+from . import personne
