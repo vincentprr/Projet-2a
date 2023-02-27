d0065af989495f4880a6b1fee3235fd63b386eae
vincentprr <furyax13@gmail.com>
2023-02-27 15:34:30 +0100

    feat(Controlers): Cleaning controlers.

--- src/controler/creneau_controler.py
+++
@@ -1,7 +0,0 @@
-from ..modeles.creneau import Creneau
-
-def get_creneau(**kwargs) -> list:
-    if len(kwargs) == 0:
-        return Creneau.query.all()
-    else:
-        return Creneau.query.filter_by(kwargs).all()
--- src/controler/hotel_controler.py
+++
@@ -1,7 +0,0 @@
-from ..modeles.hotel import Hotel
-
-def get_hotels(**kwargs) -> list:
-    if len(kwargs) == 0:
-        return Hotel.query.all()
-    else:
-        return Hotel.query.filter_by(kwargs).all()
