fc581c49b6b5a2f9c6561c33d70706e5f07b37cf
vincentprr <furyax13@gmail.com>
2023-02-27 22:40:01 +0100

    feat: Adding creation of eat.

--- src/controler/food_controleur.py
+++ src/controler/food_controleur.py
@@ -2,10 +2,15 @@ from flask_wtf import FlaskForm
 from wtforms import BooleanField
 from ..modeles.restaurant import Restaurant
 from ..modeles.repas import Repas
+from ..modeles.manger import Manger
 from ..core.database import db
+from ..controler.user_controler import get_user_by_id
 
 def get_restaurant_by_id(id_restaurant:int) -> Restaurant or None:
     return Restaurant.query.get(id_restaurant)
+
+def get_repas_by_id(id_repas:int) -> Repas or None:
+    return Repas.query.get(id_repas)
 
 def create_restaurant(name:str, openM:str, endM:str, openS:str, endS:str) -> Restaurant:
     restaurant = Restaurant(nomRest=name, ouvertureM=openM, fermetureM=endM, ouvertureS=openS, fermetureS=endS)
@@ -24,6 +29,19 @@ def create_launch(id_restaurant:int, day:str, midi:bool) -> Repas or None:
         db.session.commit()
 
     return repas
+
+def create_eat(id_mangeur:int, id_repas:int) -> Manger or None:
+    manger = None
+    mangeur = get_user_by_id(id_mangeur)
+
+    if mangeur != None and mangeur.mangeur != None:
+        repas = get_repas_by_id(id_repas)
+        if repas != None:
+            manger = Manger(idP=id_mangeur, idRepas=id_repas)
+            db.session.add(manger)
+            db.session.commit()
+
+    return manger
 
 class RepasForm(FlaskForm):
     jeudiM = BooleanField()
